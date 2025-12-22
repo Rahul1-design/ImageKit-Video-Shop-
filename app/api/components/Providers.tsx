6cc11df4b2b2af0ec340beadee67a0d78ea2bcf7
Rahul Shrestha <rahulshrestha3132@gmail.com>
2025-12-22 20:22:16 +0545

    Add Providers component with ImageKit and Session providers

--- app/api/components/Providers.tsx
+++ app/api/components/Providers.tsx
@@ -1,2 +1,12 @@
-"use client"
+import { ImageKitProvider } from "@imagekit/next";
+import { SessionProvider } from "next-auth/react";
 
+const urlEndPoint = process.env.PUBLIC_URL_ENDPOINT!;
+
+export default function Providers({ children }: { children: React.ReactNode }) {
+    return (
+        <SessionProvider>
+            <ImageKitProvider urlEndpoint={urlEndPoint}>{children}</ImageKitProvider>
+        </SessionProvider>
+    )
+}
